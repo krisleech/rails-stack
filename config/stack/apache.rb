abb9f77321667e8b52e52366bee0332456f1a6aa
Kris Leech <kris.leech@gmail.com>
2013-04-17 14:20:41 +0100

    install headers needed for caching assets

--- config/stack/apache.rb
+++ config/stack/apache.rb
@@ -1,6 +1,8 @@
 package :apache do
   apt 'apache2 apache2.2-common apache2-mpm-prefork apache2-utils libexpat1 ssl-cert apache2-prefork-dev' do
     post :install, 'a2enmod rewrite'
+    post :install, 'a2enmod headers'
+    post :install, 'a2enmod expires'
   end
 
   verify do
