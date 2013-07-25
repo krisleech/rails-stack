c89dd04d30e65111356a87f90f752ddba29b2128
Kris Leech <kris.leech@gmail.com>
2013-07-25 16:22:01 +0100

    try and figure out custom build

--- config/stack/ag.rb
+++ config/stack/ag.rb
@@ -1,5 +1,18 @@
 package :ag do
   description 'Silver Searcher (faster ack)'
-  source "https://github.com/ggreer/the_silver_searcher/archive/master.zip"
-  pre :apt, %w(automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev)
+
+  source "https://github.com/ggreer/the_silver_searcher/archive/master.zip" do
+    build_command 'build.sh'
+    install_command 'make install'
+  end
+
+  requires :ag_dependencies
+
+  verify do
+    has_executable 'ag'
+  end
+end
+
+package :ag_dependencies do
+  apt %w(automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev)
 end
