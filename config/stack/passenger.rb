2c375f21d6e76e9a335e8cb5447d57c61eefb2f9
Kris Leech <kris.leech@gmail.com>
2013-07-25 14:05:49 +0100

    configure Apache

--- config/stack/passenger.rb
+++ config/stack/passenger.rb
@@ -1,8 +1,8 @@
-package :passenger do
+package :passenger, :provides => :app_server do
 
   gem 'passenger' do
-    # sudo does not have the same PATH so we must use an absolute path
-    post :install, '/usr/local/bin/passenger-install-apache2-module --auto' 
+    version '4.0.10' # if changed, also change in passenger.conf
+    post :install, '/usr/local/bin/passenger-install-apache2-module --auto'
   end
 
   verify do
@@ -12,8 +12,23 @@ package :passenger do
   requires :ruby
   requires :apache
   requires :passenger_dependencies
+
+  optional :passenger_configuration
 end
 
 package :passenger_dependencies do
   apt 'libcurl4-gnutls-dev'
+end
+
+package :passenger_configuration do
+  description "Configure Passenger for Apache"
+  configuration_file = '/etc/apache2/apache2.conf'
+
+  push_text File.read('configurations/passenger.conf'), configuration_file, :sudo => true do
+    post :install, '/etc/init.d/apache2 restart'
+  end
+
+  verify do
+    file_contains configuration_file, 'Passenger Configuration'
+  end
 end
