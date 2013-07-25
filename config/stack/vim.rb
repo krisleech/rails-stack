fe24aec1b9aeb295e24cbfc40635b36d656cc33a
Kris Leech <kris.leech@gmail.com>
2013-07-25 16:21:33 +0100

    use turbo vim

--- config/stack/vim.rb
+++ config/stack/vim.rb
@@ -4,8 +4,12 @@ package :vim, :provides => :editor do
   runner 'rm -fr ~/.vim 2>/dev/null'
   runner 'rm -f ~/.vimrc 2>/dev/null'
 
-  runner '/usr/bin/env git clone git@github.com:krisleech/vimfiles.git ~/.vim'
+  runner '/usr/bin/env git clone git@github.com:krisleech/turbo-vim.git ~/.vim'
   runner 'ln -s ~/.vim/vimrc ~/.vimrc'
+
+  verify do
+    has_file '~/.vim/bundles.vim' # a file which does not exist in default vim
+  end
 
   requires :ack
   requires :ag
