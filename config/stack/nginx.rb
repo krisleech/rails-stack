f1f7aece87495906ae9be65f1638de93cd3c7194
Kris Leech <kris.leech@gmail.com>
2013-07-25 14:54:38 +0100

    avoid name conflict and put nginx in web_server virtual package

--- config/stack/nginx.rb
+++ config/stack/nginx.rb
@@ -1,13 +1,13 @@
-package :nginx do
+package :nginx, :provides => :web_server do
   verify do
     has_executable "nginx"
   end
 
-  requires :passenger
+  requires :nginx_passenger
 end
 
 # Nginx need passenger compiled in to it, so let passenger do it
-package :passenger do
+package :nginx_passenger do
   gem 'passenger' do
     # sudo does not have the same PATH so we must use an absolute path
     post :install, '/var/lib/gems/1.9.1/bin/passenger-install-nginx-module --auto --auto-download'
