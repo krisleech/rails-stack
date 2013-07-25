ca5a62f62b7a77deeaaa772e2c054306a975de18
Kris Leech <kris.leech@gmail.com>
2013-07-25 14:05:59 +0100

    remove old code

--- config/stack/ruby.rb
+++ config/stack/ruby.rb
@@ -1,8 +1,6 @@
 package :ruby do
   description 'Ruby 1.9.3'
 
-  binaries = %w(ruby gem irb rake)
-
   # this is Ruby 1.9.3
   apt %w(ruby1.9.1 ruby1.9.1-dev) do
     binaries.each { |bin| post :install, "ln -nfs /usr/bin/#{bin}1.9.1 /usr/bin/#{bin}"}
@@ -10,7 +8,6 @@ package :ruby do
 
   verify do
     has_executable "ruby1.9.1"
-    # binaries.each {|bin| has_symlink "/usr/bin/#{bin}1.9.1", "/usr/bin/#{bin}" }
   end
 
   requires :ruby_dependencies
