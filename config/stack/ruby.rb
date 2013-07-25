11da9f0cbf1b4e993b02332dfea621aa20ff62b0
Kris Leech <kris.leech@gmail.com>
2013-07-25 14:55:54 +0100

    ruby 1.9 already installs gem and rake

--- config/stack/ruby.rb
+++ config/stack/ruby.rb
@@ -2,9 +2,7 @@ package :ruby do
   description 'Ruby 1.9.3'
 
   # this is Ruby 1.9.3
-  apt %w(ruby1.9.1 ruby1.9.1-dev) do
-    binaries.each { |bin| post :install, "ln -nfs /usr/bin/#{bin}1.9.1 /usr/bin/#{bin}"}
-  end
+  apt %w(ruby1.9.1 ruby1.9.1-dev)
 
   verify do
     has_executable "ruby1.9.1"
