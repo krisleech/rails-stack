c6be169fc1a16700846d4af30fd9bfa9a5936aaf
Kris Leech <kris.leech@gmail.com>
2013-07-25 14:56:22 +0100

    fix locale path

--- config/stack/locale.rb
+++ config/stack/locale.rb
@@ -1,10 +1,12 @@
 package :locale do
-  pre :install, 'locale-gen en_GB.UTF-8'
+  pre :install, '/usr/sbin/locale-gen en_GB.UTF-8'
   pre :install, '/usr/sbin/update-locale LANG=en_GB.UTF-8'
-  # echo "Europe/London" > /etc/timezone
-  # sudo dpkg-reconfigure -f noninteractive tzdata
 
-  apt %w(ntp)
+  apt :ntp
+
+  verify do
+    has_apt :ntp
+  end
 
   post :install, 'ntpdate ntp.ubuntu.com'
 end
